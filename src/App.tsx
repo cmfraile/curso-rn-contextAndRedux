fcb103cde9a0504899ddca7d5367039b34521b5a
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-19 18:29:12 +0200

    añade mañana el menu lateral con el agente de usuario

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { SafeAreaView } from "react-native"
 import { AuthProvider } from "./store/context/Auth.context"
 import Main from "./routes/Main"
+import { NavigationContainer } from "@react-navigation/native"
 
 const App = () =>
-  <AuthProvider>
-    <SafeAreaView style={{flex:1,justifyContent:'center',alignItems:'center'}} >
-      <Main/>
-    </SafeAreaView>
-  </AuthProvider>
+  <NavigationContainer>
+    <AuthProvider>
+      <SafeAreaView style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+        <Main/>
+      </SafeAreaView>
+    </AuthProvider>
+  </NavigationContainer>
 
 export default App
