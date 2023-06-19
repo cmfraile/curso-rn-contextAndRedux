50178cdb4fb7ed56e5d7619bcec672a7495994a3
Carlos Manuel Fraile Gómez <carlosfrailegomez93@gmail.com>
2023-06-19 17:30:28 +0200

    135

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { SafeAreaView } from "react-native"
+import Main from "./routes/Main"
+
+const App = () =>
+  <SafeAreaView style={{flex:1,justifyContent:'center',alignItems:'center'}} >
+    <Main/>
+  </SafeAreaView>
+
+export default App
---
+++ src/routes/Main.tsx
@@ -0,0 +1,5 @@
+import { Text } from "react-native";
+
+const Main = () => <Text>MainWorks</Text>
+
+export default Main
